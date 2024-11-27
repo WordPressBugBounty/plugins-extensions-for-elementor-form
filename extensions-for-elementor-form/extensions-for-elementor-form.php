be6b2f02fc9e639bd899e0cfdec32ce5e2bfa205
Sean Murphy <sean@iamseanmurphy.com>
2024-11-27 04:18:42 +0000

    2.2

--- extensions-for-elementor-form/extensions-for-elementor-form.php
+++ extensions-for-elementor-form/extensions-for-elementor-form.php
@@ -6,9 +6,9 @@
  * Author: EduardoVillao.me
  * Author URI: https://eduardovillao.me/
  * Text Domain: extensions-for-elementor-form
- * Version: 2.0.3
+ * Version: 2.2
  * Requires at least: 5.5
- * Requires PHP: 7.0
+ * Requires PHP: 7.4
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  */
@@ -17,13 +17,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-use ElementorPro\Plugin;
-use ElementorPro\Modules\ThemeBuilder\Module;
-
 define( 'EEF_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'EEF_PLUGN_URL', plugin_dir_url( __FILE__ ) );
-define( 'EEF_VERSION', '2.0.3' );
-define( 'EEF_PHP_MINIMUM_VERSION', '7.0' );
+define( 'EEF_VERSION', '2.2' );
+define( 'EEF_PHP_MINIMUM_VERSION', '7.4' );
 define( 'EEF_WP_MINIMUM_VERSION', '5.5' );
 
 /**
